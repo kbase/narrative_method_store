ab3aa35e7fee01120ab04c4af80e9a8fd90b3e39
msneddon <mwsneddon@lbl.gov>
2014-10-13 13:18:09 -0700

    added basic test rig, need to implement

---
+++ src/us/kbase/narrativemethodstore/test/FullServerTest.java
@@ -0,0 +1,259 @@
+package us.kbase.narrativemethodstore.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.ini4j.Ini;
+import org.ini4j.Profile.Section;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import us.kbase.narrativemethodstore.NarrativeMethodStoreServer;
+
+/*
+ * These tests are specifically for testing the WS CLI written in perl.
+ * The actual tests for scripts are written in perl and can be executed individually
+ * against a WS server running on localhost on the default port set in deploy.cfg.
+ * 
+ * This class is designed to wrap all of these tests, instantiate a new temporary
+ * test WS service, and run the script tests against this service.
+ * 
+ */
+public class FullServerTest {
+	
+	final private static String TMP_FILE_SUBDIR = "tempForScriptTestRunner";
+	
+	
+	private static class ServerThread extends Thread {
+		private NarrativeMethodStoreServer server;
+		
+		private ServerThread(NarrativeMethodStoreServer server) {
+			this.server = server;
+		}
+		
+		public void run() {
+			try {
+				server.startupServer();
+			} catch (Exception e) {
+				System.err.println("Can't start server:");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//http://quirkygba.blogspot.com/2009/11/setting-environment-variables-in-java.html
+	@SuppressWarnings("unchecked")
+	private static Map<String, String> getenv() throws NoSuchFieldException,
+			SecurityException, IllegalArgumentException, IllegalAccessException {
+		Map<String, String> unmodifiable = System.getenv();
+		Class<?> cu = unmodifiable.getClass();
+		Field m = cu.getDeclaredField("m");
+		m.setAccessible(true);
+		return (Map<String, String>) m.get(unmodifiable);
+	}
+	
+	@Test
+	public void runTestServerUp() {
+		System.out.println("test...");
+	}
+	
+	
+	
+	/*
+	private static String getTestURL() {
+		int testport = SERVER.getServerPort();
+		return "http://localhost:"+testport;
+	}
+	
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		USER1 = System.getProperty("test.user1");
+		USER2 = System.getProperty("test.user2");
+		String u3 = System.getProperty("test.user3");
+		if (USER1.equals(USER2)) {
+			throw new TestException("All the test users must be unique: " + 
+					StringUtils.join(Arrays.asList(USER1, USER2, u3), " "));
+		}
+		if (USER1.equals(u3)) {
+			throw new TestException("All the test users must be unique: " + 
+					StringUtils.join(Arrays.asList(USER1, USER2, u3), " "));
+		}
+		if (USER2.equals(u3)) {
+			throw new TestException("All the test users must be unique: " + 
+					StringUtils.join(Arrays.asList(USER1, USER2, u3), " "));
+		}
+		String p1 = System.getProperty("test.pwd1");
+		String p2 = System.getProperty("test.pwd2");
+		String p3 = System.getProperty("test.pwd3");
+		
+		try {
+			AuthService.login(u3, p3);
+		} catch (Exception e) {
+			throw new TestException("Could not log in test user test.user3: " + u3, e);
+		}
+		
+		WorkspaceTestCommon.stfuLoggers();
+		
+		String tempDir = Paths.get(WorkspaceTestCommon.getTempDir())
+							.resolve(TMP_FILE_SUBDIR).toString();
+		
+		MONGO = new MongoController(WorkspaceTestCommon.getMongoExe(),
+				Paths.get(tempDir));
+		System.out.println("Using Mongo temp dir " + MONGO.getTempDir());
+		final String mongohost = "localhost:" + MONGO.getServerPort();
+		MongoClient mongoClient = new MongoClient(mongohost);
+
+		SHOCK = new ShockController(
+				WorkspaceTestCommon.getShockExe(),
+				Paths.get(tempDir),
+				u3,
+				mongohost,
+				"JSONRPCLayerHandleTest_ShockDB",
+				"foo",
+				"foo");
+		System.out.println("Using Shock temp dir " + SHOCK.getTempDir());
+
+		MYSQL = new MySQLController(
+				WorkspaceTestCommon.getMySQLExe(),
+				WorkspaceTestCommon.getMySQLInstallExe(),
+				Paths.get(tempDir));
+		System.out.println("Using MySQL temp dir " + MYSQL.getTempDir());
+		
+		HANDLE = new HandleServiceController(
+				WorkspaceTestCommon.getPlackupExe(),
+				WorkspaceTestCommon.getHandleServicePSGI(),
+				WorkspaceTestCommon.getHandleManagerPSGI(),
+				u3,
+				MYSQL,
+				"http://localhost:" + SHOCK.getServerPort(),
+				u3,
+				p3,
+				WorkspaceTestCommon.getHandlePERL5LIB(),
+				Paths.get(tempDir));
+		System.out.println("Using Handle Service temp dir " +
+				HANDLE.getTempDir());
+		
+		
+		SERVER = startupServer(mongohost,
+				mongoClient.getDB("JSONRPCLayerHandleTester"), 
+				"JSONRPCLayerHandleTester_types",
+				u3, p3);
+		int port = SERVER.getServerPort();
+		System.out.println("Started test workspace server on port " + port);
+		try {
+			CLIENT1 = new WorkspaceClient(new URL("http://localhost:" + port), USER1, p1);
+		} catch (UnauthorizedException ue) {
+			throw new TestException("Unable to login with test.user1: " + USER1 +
+					"\nPlease check the credentials in the test configuration.", ue);
+		}
+		try {
+			CLIENT2 = new WorkspaceClient(new URL("http://localhost:" + port), USER2, p2);
+		} catch (UnauthorizedException ue) {
+			throw new TestException("Unable to login with test.user2: " + USER2 +
+					"\nPlease check the credentials in the test configuration.", ue);
+		}
+		CLIENT1.setIsInsecureHttpConnectionAllowed(true);
+		CLIENT2.setIsInsecureHttpConnectionAllowed(true);
+		
+		HANDLE_CLIENT = new AbstractHandleClient(new URL("http://localhost:" +
+				HANDLE.getHandleServerPort()), USER1, p1);
+		HANDLE_CLIENT.setIsInsecureHttpConnectionAllowed(true);
+		
+	}
+	
+	private static NarrativeMethodStoreServer startupServer(
+			String mongohost,
+			DB db,
+			String typedb,
+			String handleUser,
+			String handlePwd)
+			throws InvalidHostException, UnknownHostException, IOException,
+			NoSuchFieldException, IllegalAccessException, Exception,
+			InterruptedException {
+		
+		WorkspaceTestCommon.initializeGridFSWorkspaceDB(db, typedb);
+
+		//write the server config file:
+		File iniFile = File.createTempFile("test", ".cfg",
+				new File(WorkspaceTestCommon.getTempDir()));
+		if (iniFile.exists()) {
+			iniFile.delete();
+		}
+		System.out.println("Created temporary config file: " +
+				iniFile.getAbsolutePath());
+		Ini ini = new Ini();
+		Section ws = ini.add("Workspace");
+		ws.add("mongodb-host", mongohost);
+		ws.add("mongodb-database", db.getName());
+		ws.add("backend-secret", "foo");
+		ws.add("handle-service-url", "http://localhost:" +
+				HANDLE.getHandleServerPort());
+		ws.add("handle-manager-url", "http://localhost:" +
+				HANDLE.getHandleManagerPort());
+		ws.add("handle-manager-user", handleUser);
+		ws.add("handle-manager-pwd", handlePwd);
+		ws.add("ws-admin", USER2);
+		ws.add("temp-dir", Paths.get(WorkspaceTestCommon.getTempDir())
+				.resolve(TMP_FILE_SUBDIR));
+		ini.store(iniFile);
+		iniFile.deleteOnExit();
+
+		//set up env
+		Map<String, String> env = getenv();
+		env.put("KB_DEPLOYMENT_CONFIG", iniFile.getAbsolutePath());
+		env.put("KB_SERVICE_NAME", "Workspace");
+
+		WorkspaceServer.clearConfigForTests();
+		WorkspaceServer server = new WorkspaceServer();
+		new ServerThread(server).start();
+		System.out.println("Main thread waiting for server to start up");
+		while (server.getServerPort() == null) {
+			Thread.sleep(1000);
+		}
+		return server;
+	}
+	
+	
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		if (SERVER != null) {
+			System.out.print("Killing workspace server... ");
+			SERVER.stopServer();
+			System.out.println("Done");
+		}
+		if (HANDLE != null) {
+			System.out.print("Destroying handle service... ");
+			HANDLE.destroy(WorkspaceTestCommon.getDeleteTempFiles());
+			System.out.println("Done");
+		}
+		if (SHOCK != null) {
+			System.out.print("Destroying shock service... ");
+			SHOCK.destroy(WorkspaceTestCommon.getDeleteTempFiles());
+			System.out.println("Done");
+		}
+		if (MONGO != null) {
+			System.out.print("Destroying mongo test service... ");
+			MONGO.destroy(WorkspaceTestCommon.getDeleteTempFiles());
+			System.out.println("Done");
+		}
+		if (MYSQL != null) {
+			System.out.print("Destroying mysql test service... ");
+			MYSQL.destroy(WorkspaceTestCommon.getDeleteTempFiles());
+			System.out.println("Done");
+		}
+	}
+	*/
+	
+}
---
+++ test/cfg_to_runner.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+'''
+Created on Aug 30, 2013
+
+@author: gaprice@lbl.gov
+'''
+from configobj import ConfigObj
+import os
+import sys
+
+ANT = 'ant'
+
+CFG_SECTION = 'NarrativeMethodStoreTest'
+
+CONFIG_OPTS = ['test.method-spec-git-repo',
+               'test.method-spec-git-repo-branch',
+               'test.method-spec-git-repo-local-dir',
+               'test.method-spec-git-repo-refresh-rate',
+               'test.method-spec-cache-size'
+               ]
+
+
+def write_runner(out, ant_target):
+    with open(out, 'w') as run:
+        run.write('# Generated file - do not check into git\n')
+#        run.write('cd ..\n')
+        run.write(ANT + ' ' + ant_target)
+        for o in CONFIG_OPTS:
+            if o in testcfg:
+                run.write(' -D' + o + '=' + testcfg[o])
+        run.write('\n')
+    os.chmod(out, 0755)
+    print 'Writing test runner with target "' + ant_target + '" to: ' + out
+
+
+if __name__ == '__main__':
+    d, _ = os.path.split(os.path.abspath(__file__))
+    fn = 'test.cfg'
+    if len(sys.argv) > 1:
+        fn = sys.argv[1]
+    fn = os.path.join(d, fn)
+    if not os.path.isfile(fn):
+        print 'No such config file ' + fn + '. Halting.'
+        sys.exit(1)
+    print 'Using test config file ' + fn
+    out_run_tests = os.path.join(d, 'run_tests.sh')
+    out_run_script_tests = os.path.join(d, 'run_script_tests.sh')
+    cfg = ConfigObj(fn)
+    try:
+        testcfg = cfg[CFG_SECTION]
+    except KeyError as ke:
+        print 'Test config file ' + fn + ' is missing section ' +\
+            CFG_SECTION + '. Halting.'
+        sys.exit(1)
+        
+    write_runner(out_run_tests, 'test')
+    #write_runner(out_run_script_tests, 'test-scripts')
+
+    #create a copy of the cfg file in the test/scripts/files dir for script
+    # tests -mike
+    #scriptcfgfile = os.path.join(d, 'scripts', 'files', 'test.cfg.copy')
+    #with open(scriptcfgfile, 'w') as copyfile:
+    #    cfg.write(copyfile)
